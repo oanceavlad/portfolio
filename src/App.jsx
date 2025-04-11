607beb8dccf355273c853d15667e66fa781b8f3e
Vlad <oancea.vlad19@yahoo.com>
2025-04-11 22:18:43 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <section className="h-screen bg-gradient-to-r from-blue-500 to-green-500 text-white flex justify-center items-center">
+      <h1 className="text-4xl font-semibold">Welcome to My Portfolio</h1>
+    </section>
+  );
+}
+
+export default App
